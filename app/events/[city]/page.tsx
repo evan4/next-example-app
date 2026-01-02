c2ef24ab0db296a49ce8076fc84f8dfe30a4d42b
Ivan Dedov <evan4mc@gmail.com>
2026-01-02 18:15:55 +0500

    add params in events page
    uppercase frist letter of city

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -1,7 +1,21 @@
 import H1 from "@/app/components/h1";
+import { caption } from "motion/react-client";
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+export default async function EventsPage({ params }: EventsPageProps) {
+  const { city } = await params;
+  let caption = "All Events";
+console.log(city);
+  if (city !== "all") {
+    caption = `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`;
+  }
 
-export default function EventsPage() {
-  return <main>
-    <H1>Events in </H1>
-  </main>;
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>{caption}</H1>
+    </main>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-500 text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-500 text-white overflow-y-scroll`}
       >
         <Container>
           <Header />
