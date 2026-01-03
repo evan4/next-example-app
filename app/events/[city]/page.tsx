a151b0cf16aa058e5f7c08105f14168510d71fab
Ivan Dedov <evan4mc@gmail.com>
2026-01-03 16:16:27 +0500

    create types file
    create type for events & get data from server

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -1,5 +1,5 @@
 import H1 from "@/app/components/h1";
-import { caption } from "motion/react-client";
+import { EventoEvent } from "@/app/lib/types";
 type EventsPageProps = {
   params: {
     city: string;
@@ -8,14 +8,32 @@ type EventsPageProps = {
 export default async function EventsPage({ params }: EventsPageProps) {
   const { city } = await params;
   let caption = "All Events";
-console.log(city);
-  if (city !== "all") {
+  let events: EventoEvent[] = [];
+  if (city && city !== "all") {
     caption = `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`;
+    const response = await fetch(
+      `${process.env.NEXT_PROJECT_API_URL}/course-assets/projects/evento/api/events?city=${city}`
+    );
+
+    if (response.ok) {
+      events = await response.json();
+
+      if (events.length === 0) {
+        caption = "There is no events for this city";
+      }
+    } else {
+      caption = `Sorry, there is no city in our database with name such ${city}`;
+    }
+  } else {
+    caption = "Please type the city name.";
   }
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1>{caption}</H1>
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   );
 }
