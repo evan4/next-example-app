7d4ed0b073df66c9e142f27d6a9bde4ffe64b4c7
Ivan Dedov <evan4mc@gmail.com>
2026-01-03 18:28:50 +0500

    work with EventList & EventCard component
    add css, place a photo to event in public folder

--- app/components/event-card.tsx
+++ app/components/event-card.tsx
@@ -1,10 +1,24 @@
+import Image from "next/image";
 import { EventoEvent } from "@/lib/types";
-
+import EventImage from "../../public/images/dj-set.jpg";
 type EventsListProps = {
   event: EventoEvent;
 };
 export default function EventCard({ event }: EventsListProps) {
   return (
-    <section>{event.name}</section>
-  )
+    <section className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/3 rounded-xl overflow-hidden">
+      <Image
+        src={EventImage}
+        alt={event.name}
+        width={500}
+        height={280}
+        className="h-[60%] object-fit"
+      />
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <h2 className="text-2xl font-semibold">{event.name}</h2>
+        <p className="italic text-white/75">By {event.organizerName}</p>
+        <p className="text-sm text-white/50 mt-4">By {event.location}</p>
+      </div>
+    </section>
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "bytegrad.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
