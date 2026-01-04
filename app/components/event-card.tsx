cbd7458c8aa0714c6f7c130b746abce41b6589d0
Ivan Dedov <evan4mc@gmail.com>
2026-01-04 09:54:34 +0500

    print date for event in EventCard

--- app/components/event-card.tsx
+++ app/components/event-card.tsx
@@ -6,7 +6,7 @@ type EventsListProps = {
 };
 export default function EventCard({ event }: EventsListProps) {
   return (
-    <section className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/3 rounded-xl overflow-hidden">
+    <section className="relative flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/3 rounded-xl overflow-hidden">
       <Image
         src={EventImage}
         alt={event.name}
@@ -19,6 +19,18 @@ export default function EventCard({ event }: EventsListProps) {
         <p className="italic text-white/75">By {event.organizerName}</p>
         <p className="text-sm text-white/50 mt-4">By {event.location}</p>
       </div>
+      <section className="absolute flex flex-col justify-center items-center left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rouded-md">
+        <p className="text-xl font-bold -mb-[5px]">
+          {new Date(event.date).toLocaleString("en-US", {
+            day: "2-digit",
+          })}
+        </p>
+        <p className="text-xs uppercase text-accent">
+          {new Date(event.date).toLocaleString("en-US", {
+            month: "short",
+          })}
+        </p>
+      </section>
     </section>
   );
 }
