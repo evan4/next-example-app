5eb6b18462ace117c365aabcee08e9add353e39e
Ivan Dedov <evan4mc@gmail.com>
2026-01-04 10:01:53 +0500

    add link to event in event card componet

--- app/components/event-card.tsx
+++ app/components/event-card.tsx
@@ -1,36 +1,42 @@
 import Image from "next/image";
 import { EventoEvent } from "@/lib/types";
 import EventImage from "../../public/images/dj-set.jpg";
+import Link from "next/link";
 type EventsListProps = {
   event: EventoEvent;
 };
 export default function EventCard({ event }: EventsListProps) {
   return (
-    <section className="relative flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/3 rounded-xl overflow-hidden">
-      <Image
-        src={EventImage}
-        alt={event.name}
-        width={500}
-        height={280}
-        className="h-[60%] object-fit"
-      />
-      <div className="flex flex-col flex-1 justify-center items-center">
-        <h2 className="text-2xl font-semibold">{event.name}</h2>
-        <p className="italic text-white/75">By {event.organizerName}</p>
-        <p className="text-sm text-white/50 mt-4">By {event.location}</p>
-      </div>
-      <section className="absolute flex flex-col justify-center items-center left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rouded-md">
-        <p className="text-xl font-bold -mb-[5px]">
-          {new Date(event.date).toLocaleString("en-US", {
-            day: "2-digit",
-          })}
-        </p>
-        <p className="text-xs uppercase text-accent">
-          {new Date(event.date).toLocaleString("en-US", {
-            month: "short",
-          })}
-        </p>
+    <Link
+      className="flex-1 basis-80 h-[380px] max-w-[500px]"
+      href={`/event/${event.slug}`}
+    >
+      <section className="relative w-full h-full flex flex-col bg-white/3 rounded-xl overflow-hidden transition hover:scale-105 active:scale-[1.02]">
+        <Image
+          src={EventImage}
+          alt={event.name}
+          width={500}
+          height={280}
+          className="h-[60%] object-fit"
+        />
+        <div className="flex flex-col flex-1 justify-center items-center">
+          <h2 className="text-2xl font-semibold">{event.name}</h2>
+          <p className="italic text-white/75">By {event.organizerName}</p>
+          <p className="text-sm text-white/50 mt-4">By {event.location}</p>
+        </div>
+        <section className="absolute flex flex-col justify-center items-center left-3 top-3 h-[45px] w-[45px] bg-black/30 rouded-md">
+          <p className="text-xl font-bold -mb-[5px]">
+            {new Date(event.date).toLocaleString("en-US", {
+              day: "2-digit",
+            })}
+          </p>
+          <p className="text-xs uppercase text-accent">
+            {new Date(event.date).toLocaleString("en-US", {
+              month: "short",
+            })}
+          </p>
+        </section>
       </section>
-    </section>
+    </Link>
   );
 }
